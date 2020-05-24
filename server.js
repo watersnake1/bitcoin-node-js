e7b3074e667176de78371a5f327e6305244626b4
christian <mrmurray253@gmail.com>
2020-05-23 22:50:32 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const express = require("express");
+const bodyParser = require("body-parser");
+const rpcMethods = require("./routes/api");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use("/api", rpcMethods);
+
+const port = process.env.PORT || 4444;
+
+server = app.listen(port, () => console.log(`server running on port ${port}`));
+
